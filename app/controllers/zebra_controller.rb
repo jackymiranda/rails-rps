c6d46bc76da6f35dcfcd61b725dc8acf341e1f6d
jackymiranda <jackelyn.miranda@chicagobooth.edu>
2024-10-25 20:58:23 +0000

    tarea rails

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,21 @@
+class ZebraController < ApplicationController
+  def giraffe
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_rock" })
+  end
+
+
+  def elephant
+    @random_move = ["rock", "paper", "scissors"].sample
+    
+    render({ :template => "game_templates/play_paper" })
+  end
+
+
+  def lion
+    @random_move = ["rock", "paper", "scissors"].sample
+    
+    render({ :template => "game_templates/play_scissors" })
+  end
+end
